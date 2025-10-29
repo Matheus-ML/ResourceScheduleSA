60a3542fe7ca2e028e319a5c35c4f3a439ba3454
matheus_leite150 <matheusleite1216@gmail.com>
2025-10-29 19:37:41 -0300

    Inicio do projetinho wow

---
+++ ResourceScheduleSA/ResourceScheduleSA/src/main/java/com/senai/ResourceScheduleSA/ResourceScheduleSaApplication.java
@@ -0,0 +1,13 @@
+package com.senai.ResourceScheduleSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceScheduleSaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceScheduleSaApplication.class, args);
+	}
+
+}
---
+++ ResourceScheduleSA/ResourceScheduleSA/src/test/java/com/senai/ResourceScheduleSA/ResourceScheduleSaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.ResourceScheduleSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceScheduleSaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
