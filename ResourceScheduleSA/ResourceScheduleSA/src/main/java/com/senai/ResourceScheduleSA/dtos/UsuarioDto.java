7828889baf99c6d66fa27ed8ba2cab4ceed74df4
matheus_leite150 <matheusleite1216@gmail.com>
2025-10-30 19:06:54 -0300

    usuarioDto

--- ResourceScheduleSA/ResourceScheduleSA/src/main/java/com/senai/ResourceScheduleSA/dtos/UsuarioDto.java
+++ ResourceScheduleSA/ResourceScheduleSA/src/main/java/com/senai/ResourceScheduleSA/dtos/UsuarioDto.java
@@ -1,4 +1,24 @@
 package com.senai.ResourceScheduleSA.dtos;
 
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDto {
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+    private String matricula;
+
+    private LocalDate data;
 }
