176879b37ded5817b5d13e5c1df31576c87e83fa
matheus_leite150 <matheusleite1216@gmail.com>
2025-10-30 18:53:37 -0300

    RecursoDTO

--- ResourceScheduleSA/ResourceScheduleSA/src/main/java/com/senai/ResourceScheduleSA/dtos/RecursoDto.java
+++ ResourceScheduleSA/ResourceScheduleSA/src/main/java/com/senai/ResourceScheduleSA/dtos/RecursoDto.java
@@ -1,8 +1,33 @@
 package com.senai.ResourceScheduleSA.dtos;
 
 
+import com.senai.ResourceScheduleSA.models.DiaDisponivel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RecursoDto {
 
+    private Long id;
 
+    private String descricao;
+
+    private String tipo;
+
+    private DiaDisponivel diaDisponivel;
+
+    private LocalDate dataInicio;
+
+    private LocalDate dataFinal;
+
+    private LocalTime horaInicio;
+
+    private LocalTime horaFinal;
 
 }
--- ResourceScheduleSA/ResourceScheduleSA/src/main/java/com/senai/ResourceScheduleSA/services/RecursoService.java
+++ ResourceScheduleSA/ResourceScheduleSA/src/main/java/com/senai/ResourceScheduleSA/services/RecursoService.java
@@ -1,5 +1,7 @@
 package com.senai.ResourceScheduleSA.services;
 
+import com.senai.ResourceScheduleSA.dtos.RecursoDto;
+import com.senai.ResourceScheduleSA.models.RecursoModel;
 import com.senai.ResourceScheduleSA.repositories.RecursoRepository;
 import org.springframework.stereotype.Service;
 
