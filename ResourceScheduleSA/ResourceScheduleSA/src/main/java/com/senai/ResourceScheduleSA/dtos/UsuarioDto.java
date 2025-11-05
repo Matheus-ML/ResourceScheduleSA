58a2b10a3713807146ecb139a403ce0c7cdd8bb5
BrunoRprogm <brunoramonnn986@gmail.com>
2025-11-04 20:57:11 -0400

    Implementado REGEX no usuarioDto, validando a senha do usuário /falta tratamento

--- ResourceScheduleSA/ResourceScheduleSA/src/main/java/com/senai/ResourceScheduleSA/dtos/UsuarioDto.java
+++ ResourceScheduleSA/ResourceScheduleSA/src/main/java/com/senai/ResourceScheduleSA/dtos/UsuarioDto.java
@@ -3,6 +3,7 @@ package com.senai.ResourceScheduleSA.dtos;
 import jakarta.persistence.Column;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,7 +19,10 @@ public class UsuarioDto {
     private String nome;
 
     private String email;
-
+    @Pattern(
+            regexp = "^[A-Za-z0-9]{5,}$",
+            message = "O nome de usuário deve ter pelo menos 5 caracteres e conter apenas letras e números. "
+    )
     private String senha;
 
     private String matricula;
